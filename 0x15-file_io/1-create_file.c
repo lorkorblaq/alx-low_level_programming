e697b62b420ae263ff84699abba9deea2ee681fc
blaq <f3mioloko@gmail.com>
2023-04-15 03:36:07 +0100

    q

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,11 +18,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-
-	for (i = 0; text_content[i]; i++);
+	for (i = 0; text_content[i]; i++)
+	{
 	fwrite = write(file, text_content, i);
 	if (fwrite == -1)
 		return (-1);
+	}
 	}
 	close(file);
 	return (1);
